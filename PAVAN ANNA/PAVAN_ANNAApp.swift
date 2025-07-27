caf514db5e25636bfe4d17f718f8b7061bfa8262
dirakhil <dir.akhilmaddali@gmail.com>
2025-07-27 16:52:25 +0530

    Initial Commit

---
+++ PAVAN ANNA/PAVAN_ANNAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PAVAN_ANNAApp.swift
+//  PAVAN ANNA
+//
+//  Created by Akhil Maddali on 27/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PAVAN_ANNAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PAVAN ANNATests/PAVAN_ANNATests.swift
@@ -0,0 +1,17 @@
+//
+//  PAVAN_ANNATests.swift
+//  PAVAN ANNATests
+//
+//  Created by Akhil Maddali on 27/07/25.
+//
+
+import Testing
+@testable import PAVAN_ANNA
+
+struct PAVAN_ANNATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
